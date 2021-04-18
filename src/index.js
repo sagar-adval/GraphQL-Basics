93828c986222f22b747838bb4c1ea616b645cc02
Sagar Adval <sagaradval@gmail.com>
2021-04-18 15:20:38 +0530

    Basic typeDefs and resolvers added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("Hello GraphQL");
+import { GraphQLServer } from "graphql-yoga";
+
+//Type definitions (Schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+    }
+`;
+
+//Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "First query";
+    },
+    name() {
+      return "Sagar Adval";
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("Server started!");
+});
